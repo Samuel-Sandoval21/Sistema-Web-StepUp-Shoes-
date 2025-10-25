bd39a305720786c1bc283984ce5600c42402c8f8
Adbeel99 <103293313+Adbeel99@users.noreply.github.com>
2025-10-25 12:32:45 -0600

    Primer commit

---
+++ stepupshoes/src/test/java/com/stepup/shoes/ShoesApplication/ShoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stepup.shoes.ShoesApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
